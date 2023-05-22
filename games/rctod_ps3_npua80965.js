13bc9233ed85741a7f8158c7f3549c4a0ff6e04a
Tristan Gauci <tristan@maega.com.au>
2023-05-23 01:31:28 +1000

    Added partial support for R&C Tools of Destruction

---
+++ games/rctod_ps3_npua80965.js
@@ -0,0 +1,23 @@
+/*
+    * Memory address reference object
+    * Applies to Ratchet & Clank: Tools of Destruction (PS3) [NPUA80965]
+*/
+
+export default {
+    bigEndian: true,
+    nanotech: [null, 'uint32'],
+    bolts: [0x31020C28C, 'uint32'],
+    multiplier: null, // 1 byte, max 255 multiplier.
+    raritanium: [0x31020C290, 'uint32'],
+    mode: [null, 'uint32'],
+    state: [null, 'uint32'],
+    posBase: null,
+    galacticMapBase: null,
+    current: {
+        item: [null, 'uint8'],
+        planet: [null, 'uint8']
+    },
+    weapons: {},
+    gadgets: {},
+    planets: {}
+};
